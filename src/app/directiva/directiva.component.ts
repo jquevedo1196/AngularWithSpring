28163e4b5ff47027ceee408015bfad41d4d02746
Jesus Quevedo <jenrique_qt@hotmail.com>
2019-05-03 16:21:13 -0500

    agregando primeros pasos for e if

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directiva',
   templateUrl: './directiva.component.html',
-  styleUrls: ['./directiva.component.css']
 })
-export class DirectivaComponent implements OnInit {
-
+export class DirectivaComponent {
+  listaCurso: string[] = ['TypeScrip', 'JavaScript', 'Java SE', 'C#', 'Angular'];
+  habilitar = true;
   constructor() { }
-
-  ngOnInit() {
+  setHabilitar(): void {
+    this.habilitar = this.habilitar == true ? false : true;
   }
-
 }
